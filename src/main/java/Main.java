61bddbbb35ad3bdad259bf26556e2d589cfa75fa
thor <thor@cphbusiness.dk>
2020-08-24 11:18:55 +0200

    fodbold ud

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         /*
-        lav fodboldklub-objekt
          lav en liste af personer med random-valideret cprnummer.
          - ud fra en liste af navne læses fra fil
          for hvert navn:
